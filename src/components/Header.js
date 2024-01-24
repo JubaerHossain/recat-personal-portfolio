48de578029415bd1a0e46afd1fd91c6782c444e8
Jubaer Hossain <jubaer01.cse@gmail.com>
2024-01-24 17:29:15 +0600

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
       <div className='container mx-auto h-full flex items-center justify-between'>
         {/* logo */}
         <a href='#'>
-          <img src={Logo} alt='' />
+          Jubaer Hossain
         </a>
         {/* nav */}
         <div className='hidden lg:block'>
