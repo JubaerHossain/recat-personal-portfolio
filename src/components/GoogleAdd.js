c40e4348b71f19b0be40528771efb53f857ba41f
Jubaer Hossain <jubaer01.cse@gmail.com>
2024-01-24 17:42:02 +0600

    done

--- src/components/GoogleAdd.js
+++ src/components/GoogleAdd.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const GoogleAdd = () => {
   useEffect(() => {
     // If you have script tags, append them to the head of the document
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.async = true;
-    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4152205468557848';
-    script.crossOrigin = 'anonymous';
+    script.src =
+      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4152205468557848";
+    script.crossOrigin = "anonymous";
     document.head.appendChild(script);
 
     // Trigger the Adsbygoogle push when the component mounts
@@ -14,14 +15,18 @@ const GoogleAdd = () => {
   }, []);
 
   return (
-            <ins
-              className='adsbygoogle'
-              style={{ display: 'block' }}
-              data-ad-client='ca-pub-4152205468557848'  
-              data-ad-slot='8835529607'  
-              data-ad-format='auto'
-              data-full-width-responsive='true'
-            ></ins>
+    <section className="bg-tertiary py-12">
+      <div className="container mx-auto">
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-4152205468557848"
+          data-ad-slot="8835529607"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </div>
+    </section>
   );
 };
 
